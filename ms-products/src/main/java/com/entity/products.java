37db4a995fafd87a01d0338d1f1de9f419e643b5
Eduardo MArtinez Juarez <eduard.maju@gmail.com>
2026-02-16 16:15:54 -0600

    Creación de entidad products

---
+++ ms-products/src/main/java/com/entity/products.java
@@ -0,0 +1,24 @@
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String code;
+
+    private BigDecimal price;
+
+    private Integer stock;
+
+}
