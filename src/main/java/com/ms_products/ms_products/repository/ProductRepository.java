543aa0a3923306a458c2ad4986b5d3e86f6bd852
AngelePro <gabriel.martinez.ids.mx.com>
2026-02-16 20:01:05 -0600

    REPO, SER

---
+++ src/main/java/com/ms_products/ms_products/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.ms_products.ms_products.repository;
+
+import com.ms_products.ms_products.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+
+    Optional<ProductEntity> findByCode(String code);
+
+}
