1cb5e52006bef731fc8bf3a212b0e342a4fce46a
Eduardo MArtinez Juarez <eduard.maju@gmail.com>
2026-02-20 12:55:52 -0600

    tests

--- target/generated-sources/annotations/com/ms_products/mapper/ProductMapperImpl.java
+++ target/generated-sources/annotations/com/ms_products/mapper/ProductMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-02-20T11:57:05-0600",
+    date = "2026-02-20T12:52:54-0600",
     comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.18 (Eclipse Adoptium)"
 )
 @Component
